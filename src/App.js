48cb5ae42824789b7c19212f1d4a70714a536fff
PalashDas11 <pnpalash20@gmail.com>
2022-05-22 02:21:23 +0600

    using tailwind and create navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+import NotFound from './component/NotFound/NotFound';
+import Tools from './component/Tools/Tools';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header></Header>
+     <Routes>
+       <Route path="/" element ={<Home/>}></Route>
+       <Route path="/tools" element ={<Tools/>}></Route>
+       <Route path="/tools" element ={<Tools/>}></Route>
+       <Route path="*" element ={<NotFound/>}></Route>
+     </Routes>
+     
     </div>
   );
 }
